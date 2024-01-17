ff3feb38e321f23c2841fab3dabede1e750f5b03
Mohammed Hussen <99973399+awash-dev@users.noreply.github.com>
2024-01-17 08:49:30 +0300

    Add files via upload
    this website designed by Awash dev

---
+++ script/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $(".filter-item").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".post-box").show("1000");
+    } else {
+      $(".post-box")
+        .not("." + value)
+        .hide("1000");
+      $(".post-box")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+  //   active onclik button
+  $(".filter-item").click(function () {
+    $(this).addClass("active-filter").siblings().removeClass("active-filter");
+  });
+});
+
+// nav background
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0)
+})
